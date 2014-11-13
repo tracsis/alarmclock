0929ca8ad299840f0fed57cf60150b6b95f8401c
David Turner <d.turner@tracsis.com>
2014-11-13 09:39:23 +0000

    Test now contains some example code

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,19 @@
 module Main (main) where
 
+import Control.Applicative
+import Control.Concurrent
+import Control.Concurrent.AlarmClock
+import Control.Exception
+import Data.Time
+
+alarmAction :: IO (Maybe UTCTime)
+alarmAction = do
+  t <- getCurrentTime
+  putStrLn $ "Alarm occurred at " ++ show t
+  return $ Just $ addUTCTime 5 t
+
 main :: IO ()
-main = return ()
+main = bracket (mkAlarmClock alarmAction) destroyAlarmClock $ \ac -> do
+  t <- getCurrentTime
+  setAlarm ac $ addUTCTime 2 t
+  threadDelay 18000000
